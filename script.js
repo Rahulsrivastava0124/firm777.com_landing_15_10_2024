6d67ae73efd8297e9b40bb7385c467096f77ab3c
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-12-10 12:52:50 +0000

    change the script.js file

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ axios
     phoneData = result?.data?.phone;
     let anchors = document.querySelectorAll("a");
     anchors.forEach(function (anchor) {
-      if (Promocode != undefined) {
+      if (Promocode!=undefined) {
         console.log(Promocode[1]);
         anchor.href = `https://api.whatsapp.com/send?phone=${result?.data?.phone}&text=Hi%2C%20I%20Need%20ID%20,CODE%20-${Promocode[1]}`;
-        document.getElementById("PromoCode").value = Promocode[1];
+        document.getElementById("PromoCode").value=Promocode[1];
         document.getElementById("PromoCode").disabled = true;
+
       } else {
         anchor.href = `https://api.whatsapp.com/send?phone=${result?.data?.phone}&text=Hi%2C%20I%20Need%20ID%20,CODE%20-FREE50`;
       }
@@ -31,34 +32,28 @@ const submitForm = (event) => {
 
   let url = `https://api.whatsapp.com/send?phone=${phoneData}&text=Name-%20${name}%0APhone-${phone}%2C%0APromocode-${PromoCode}`;
 
-  axios
-    .post("https://api-firm777-com.onrender.com/createUser", {
-      name,
-      phone,
-      promocode: PromoCode,
-    })
-    .then((result) => {
-      axios
-        .post(
-          "https://script.google.com/macros/library/d/1uoW_RsNFfUnSvqauiAdif5zK55TwESkioPRNlnb9MCUEVCB_sDGwOnGA/2",
-          {
-            Name: name,
-            Phone: phone,
-            Promocode: PromoCode,
-            Date: Date,
-          }
-        )
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  axios.post("https://api-firm777-com.onrender.com/createUser",{
+    name,
+    phone,
+    promocode:PromoCode,
+  }).then(result => {
+    axios.post('https://script.google.com/macros/library/d/1uoW_RsNFfUnSvqauiAdif5zK55TwESkioPRNlnb9MCUEVCB_sDGwOnGA/2',{
+      Name:name,
+      Phone:phone,
+      Promocode:PromoCode,
+      Date:Date,
+    }).then(result => {
       console.log(result);
-      document.getElementById("SubmitIndex").innerHTML = "Loding...";
-      return window.location.replace(url);
-    })
-    .catch((err) => {
+    }).catch((err) => {
       console.log(err);
-    });
+    })
+    console.log(result);
+    document.getElementById('SubmitIndex').innerHTML="Loding...";
+    return window.location.replace(url);
+  }).catch((err) => {
+    console.log(err)
+  })
+
+
+
 };
