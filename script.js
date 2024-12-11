db6ba501f8fa15ce8376dc47c79d0ce91ec51ca9
Rahul srivastava <87309230+Rahulsrivastava0124@users.noreply.github.com>
2024-12-11 07:00:50 +0000

    `Updated form777.html and script.js to modify promo code input and form submission logic.`

--- script.js
+++ script.js
@@ -3,29 +3,26 @@ let Promocode = UserProme[1]?.split("=");
 
 var phoneData;
 axios
-    .get("https://api.astropoints.in/auth/getNumber?id=1")
-    .then((result) => {
-      phoneData = result?.data?.phone;
-      let anchors = document.querySelectorAll("a");
-      anchors.forEach(function (anchor) {
-
-        if (Promocode!=undefined) {
-          console.log(Promocode[1]);
-          anchor.href = `https://api.whatsapp.com/send?phone=${result?.data?.phone}&text=Hi%2C%20I%20Need%20ID%20,CODE%20-${Promocode[1]}`;
-          if (document.getElementById("PromoCode")) {
-            document.getElementById("PromoCode").value=Promocode[1];
-            document.getElementById("PromoCode").disabled = true;
-          }
-
-
-        } else {
-          anchor.href = `https://api.whatsapp.com/send?phone=${result?.data?.phone}&text=Hi%2C%20I%20Need%20ID%20,CODE%20-FREE50`;
+  .get("https://api.astropoints.in/auth/getNumber?id=1")
+  .then((result) => {
+    phoneData = result?.data?.phone;
+    let anchors = document.querySelectorAll("a");
+    anchors.forEach(function (anchor) {
+      if (Promocode != undefined) {
+        console.log(Promocode[1]);
+        anchor.href = `https://api.whatsapp.com/send?phone=${result?.data?.phone}&text=Hi%2C%20I%20Need%20ID%20,CODE%20-${Promocode[1]}`;
+        if (document.getElementById("PromoCode")) {
+          document.getElementById("PromoCode").value = Promocode[1];
+          document.getElementById("PromoCode").disabled = true;
         }
-      });
-    })
-    .catch((err) => {
-      console.log(err);
+      } else {
+        anchor.href = `https://api.whatsapp.com/send?phone=${result?.data?.phone}&text=Hi%2C%20I%20Need%20ID%20,CODE%20-FREE50`;
+      }
     });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const submitForm = (event) => {
   event.preventDefault();
@@ -48,20 +45,54 @@ const submitForm = (event) => {
   //   },
   // })
 
-  axios.post("https://api-firm777-com.onrender.com/createUser",{
-    name,
-    phone,
-    promocode:PromoCode,
-  }).then(result => {
-   
-    console.log(result);
-    document.getElementById('SubmitIndex').innerHTML="Loding...";
-    // return window.location.replace(url);
-  }).catch((err) => {
-    console.log(err)
-  })
+
+  const data = {
+    Name: name,
+    Phone: phone,
+    Promocode: PromoCode,
+
+};
+
+const formData = new FormData();
+
+for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+        formData.append(key, data[key]);
+    }
+}
+
+  fetch(
+    "https://script.google.com/macros/s/AKfycbzAQnNKAKiUEjnK-smHTx1crstgvetlvv5sDnAMoybCZ7Ydl8M9x7hEUlJY6zKUOx_5xw/exec",
+    {
+      method: "POST",
+      mode: "no-cors",
+      body: formData,
+      headers: {
+        "Content-Type": "application/json", // <--- Correct Content-Type
+      },
+    }
+  )
+    .then((res) => res.text())
+    .then((data) => {
+      document.getElementById("SubmitIndex").innerHTML = "Loding...";
+      return window.location.replace(url);
+    })
+    .catch((err) => console.log(err));
+
+  // axios.post("https://api-firm777-com.onrender.com/createUser",{
+  //   name,
+  //   phone,
+  //   promocode:PromoCode,
+  // }).then(result => {
+
+  //   console.log(result);
+  //   document.getElementById('SubmitIndex').innerHTML="Loding...";
+  //   return window.location.replace(url);
+  // }).catch((err) => {
+  //   console.log(err)
+  // })
 };
 
 function GoLanding() {
-  window.location.href='./form777.html'
+  window.location.href = "./form777.html";
 }
